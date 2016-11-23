3f4820dfbb87ee662890c472392a31b6b9441405
MickPlaYer <mickgamehome@gmail.com>
2016-11-23 16:19:37 +0800

    Set up redirect when user not login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      respond_to do |format|
+        format.html { redirect_to '/' }
+        format.json { super }
+      end
+    end
+  end
 end
