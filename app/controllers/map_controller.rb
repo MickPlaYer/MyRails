4e843d53b44e7b4eae2f8a4cdfbe15a3fefbd34d
MickPlaYer <mickgamehome@gmail.com>
2016-12-06 20:28:27 +0800

    Change view

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,7 @@ class MapController < ApplicationController
   def index
     respond_to do |format|
       format.json do
-        @items = Item.all
+        @items = Item.all if @item.nil?
         items = []
         3.times do
           items << @items.sample.id
