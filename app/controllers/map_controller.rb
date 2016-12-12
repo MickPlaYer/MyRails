49c8952fdf76bae122610742e18e014d1b40a0b8
MickPlaYer <mickgamehome@gmail.com>
2016-12-12 19:42:55 +0800

    Map list

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,14 +1,23 @@
 class MapController < ApplicationController
+  before_action :authenticate_admin_privilege!, except: :index
+
   def index
     respond_to do |format|
-      format.json do
-        @items = Item.all if @item.nil?
-        items = []
-        3.times do
-          items << @items.sample.id
-        end
-        render json: { items: items }
+      format.html { authenticate_admin_privilege! }
+      format_json format
+    end
+  end
+
+  private
+
+  def format_json format
+    format.json do
+      @items = Item.all if @item.nil?
+      items = []
+      3.times do
+        items << @items.sample.id
       end
+      render json: { items: items }
     end
   end
 end
